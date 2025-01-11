dd31cda535b8a84047d55d585e568a689c0dd97d
Josiah137 <eyosiyas73@gmail.com>
2025-01-11 12:51:05 +0300

    it works now perfectly. i have not set any validation and the backend logic yet but it creates, Lists, updates,and deletes books. instade of applying everything at once, to trace errors it is better to develop it little by little. first the testing my api noramlly works, the setting some restictions like validations, then setting authtication and authorization etc. now even on post man with out no authtication it works

---
+++ Library Management System API/library/urls.py
@@ -0,0 +1,8 @@
+#importing the path module and my views
+from django.urls import path
+from .views import BookListCreateView, BookDetailView
+
+urlpatterns = [
+    path('books/', BookListCreateView.as_view(), name='book-list-create'),
+    path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
+]
--- Library Management System API/library/views.py
+++ Library Management System API/library/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
+
+#handling the CRUD
+
+# TO LIst all books or create a new book 
+class BookListCreateView(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+# Retrieve, update, or delete a single book
+class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
